06ba834dd2bbfb5530c6a92eff107ba72e337aa5
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-05-21 22:10:25 +0100

    added upload endpoint for images

--- server/index.js
+++ server/index.js
@@ -22,14 +22,6 @@ mongoose.connect(
 app.get("/", (req, res) => {
   res.send("Express app is running");
 });
-// Start Server
-app.listen(port, (error) => {
-  if (!error) {
-    console.log("Server Running on Port " + port);
-  } else {
-    console.log("Error : " + error);
-  }
-});
 
 //Image Storage Engine
 
@@ -41,4 +33,26 @@ const storage = multer.diskStorage({
       `${file.fieldname}_${Date.now()}_${path.extname(file.originalname)}`
     );
   },
+});
+
+const upload = multer({ storage: storage });
+
+// Creating Upload Endpoint for images
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
+// Schema for Creating Products
+
+// Start Server
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server Running on Port " + port);
+  } else {
+    console.log("Error : " + error);
+  }
 });
